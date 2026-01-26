24bd849feef00df24980051ec84cd8ef350bc7e3
Troy Dutton <troy@modernintelligence.ai>
2026-01-26 14:49:14 -0600

    Implement matcher and criterion

--- src/criterion/criterion.py
+++ src/criterion/criterion.py
@@ -1,17 +1,130 @@
+from __future__ import annotations
+import torch.nn.functional as F
+from torchvision.ops.boxes import generalized_box_iou, box_convert, box_iou
 from torch import nn, Tensor
-from typing import Dict, List, Tuple
+from typing import Optional, Tuple
+from typing import Dict, List, TYPE_CHECKING
+import torch
 from utils.misc import take_annotation_from
+from criterion.matcher import HungarianMatcher
+
+if TYPE_CHECKING:
+    from criterion.matcher import Matches
+
+Targets = List[Dict[str, Tensor]]
 
 
 class SetCriterion(nn.Module):
-    """ """
+    """
 
-    def __init__(self) -> None:
+    Args:
+        loss_weights: Weights for different loss components.
+        class_weights: Weights for each class, optional.
+        alpha: Focal loss alpha parameter, optional.
+        gamma: Focal loss gamma parameter, optional.
+    """
+
+    def __init__(
+        self,
+        loss_weights: Dict[str, float],
+        class_weights: Optional[Tensor] = None,
+        alpha: float = 0.25,
+        gamma: float = 2.0,
+    ) -> None:
         super().__init__()
 
-    def forward(self, predictions: Dict[str, Tensor], targets: List[Dict[str, Tensor]]) -> Dict[str, Tensor]:
-        """ """
-        pass
+        self.loss_weights = loss_weights
+        self.class_weights = class_weights
+        self.alpha = alpha
+        self.gamma = gamma
+
+        self.matcher = HungarianMatcher(cost_weights=loss_weights, alpha=alpha, gamma=gamma)
+
+    def forward(self, predictions: Dict[str, Tensor], targets: Targets) -> Dict[str, Tensor]:
+        """
+        Calculates losses for the given predictions and targets.
+
+        Args:
+            predictions: Model predictions, with keys:
+                - `logits`: Class logits of shape (batch_size, num_queries, num_classes).
+                - `boxes`: Predicted bounding boxes of shape (batch_size, num_queries, 4).
+            targets: List of targets for each image, with keys:
+                - `labels`: Target class labels of shape (num_targets,).
+                - `boxes`: Target bounding boxes of shape (num_targets, 4).
+
+        Returns:
+            losses: Dictionary of calculated losses.
+        """
+        matched_indices = self.matcher(predictions, targets)
+
+        box_loss, giou_loss = self.calculate_box_losses(predictions, targets, matched_indices)
+        class_loss = self.calculate_class_loss(predictions, targets, matched_indices)
+
+        # Weighted sum
+        losses = {"box": box_loss, "giou": giou_loss, "class": class_loss}
+        losses["overall"] = sum(v * self.loss_weights.get(k, 1) for k, v in losses.items())
+
+        return losses
+
+    def calculate_box_losses(self, predictions: Dict[str, Tensor], targets: Targets, matched_indices: Matches) -> Tuple[Tensor, Tensor]:
+        """
+        Calculate L1 and GIoU loss between matched predictions and target boxes.
+
+        Args:
+            predictions: Model predictions, with keys:
+                - `boxes`: Predicted bounding boxes of shape (batch_size, num_queries, 4).
+
+        """
+
+        # Select the matched predictions and targets
+        batch_indices = torch.cat([torch.full_like(indices, i) for i, (indices, _) in enumerate(matched_indices)])
+        query_indices = torch.cat([indices for (indices, _) in matched_indices])
+        prediction_indices = (batch_indices, query_indices)
+
+        prediction_boxes = predictions["boxes"][prediction_indices]
+        target_boxes = torch.cat([t["boxes"][indices] for t, (_, indices) in zip(targets, matched_indices)])
+        num_targets = target_boxes.shape[0]
+
+        # Minimize L1 Distance
+        box_loss = F.l1_loss(prediction_boxes, target_boxes, reduction="sum")
+        box_loss = box_loss / num_targets
+
+        prediction_boxes = box_convert(prediction_boxes, "cxcywh", "xyxy")
+        target_boxes = box_convert(target_boxes, "cxcywh", "xyxy")
+
+        # Maximize GIoU
+        giou_loss = (1 - generalized_box_iou(prediction_boxes, target_boxes).diag()).sum()
+        giou_loss = giou_loss / num_targets
+
+        return box_loss, giou_loss
+
+    def calculate_class_loss(self, predictions: Dict[str, Tensor], targets: Targets, matched_indices: Matches) -> Tensor:
+        # Select the matched predictions and targets
+        batch_indices = torch.cat([torch.full_like(src, i) for i, (src, _) in enumerate(matched_indices)])
+        query_indices = torch.cat([indices for (indices, _) in matched_indices])
+
+        prediction_logits = predictions["logits"]  # (batch_size, num_queries, num_classes)
+
+        target_labels = torch.cat([t["labels"][i] for t, (_, i) in zip(targets, matched_indices)])  # (num_targets)
+        num_targets = target_labels.shape[0]
+
+        # One-hot encode target labels
+        one_hot_target_labels = torch.zeros_like(prediction_probs)
+        one_hot_target_labels[batch_indices, query_indices, target_labels] = 1.0
+
+        # We use binary focal loss for classification
+        prediction_probs = prediction_logits.sigmoid()
+
+        # Numerically stable version of α * (1 - prob) ** γ * -prob.log()
+        pos_class_cost = self.alpha * ((1 - prediction_probs) ** self.gamma) * (-F.logsigmoid(prediction_logits))
+
+        # Numerically stable version of (1 - α) * prob ** γ * -(1 - prob).log()
+        neg_class_cost = (1 - self.alpha) * (prediction_probs**self.gamma) * (-F.logsigmoid(-prediction_logits))
+
+        class_loss = pos_class_cost * one_hot_target_labels + neg_class_cost * (1 - one_hot_target_labels)
+        class_loss = class_loss.sum() / num_targets
+
+        return class_loss
 
     @take_annotation_from(forward)
     def __call__(self, *args, **kwargs):
--- src/criterion/matcher.py
+++ src/criterion/matcher.py
@@ -1,21 +1,136 @@
+from __future__ import annotations
 from torch import nn, Tensor
-from typing import Dict, List, Tuple
+from torch.nn import functional as F
+from typing import Dict, List, Tuple, TYPE_CHECKING
 from utils.misc import take_annotation_from
+from torchvision.ops.boxes import box_convert, generalized_box_iou
+from scipy.optimize import linear_sum_assignment
 
 import torch
 
+if TYPE_CHECKING:
+    from criterion.criterion import Targets
+
+Matches = List[Tuple[Tensor, Tensor]]
+
 
 class HungarianMatcher(nn.Module):
-    """ """
+    """
+    Hungarian Matching algorithm for Object Detection tasks.
 
-    def __init__(self) -> None:
+    Args:
+        cost_weights: Weights for cost components (`class`, `box`, `giou`).
+        alpha: Focal loss alpha parameter, optional.
+        gamma: Focal loss gamma parameter, optional.
+    """
+
+    def __init__(self, cost_weights: Dict[str, float], alpha: float = 0.25, gamma: float = 2.0) -> None:
         super().__init__()
 
-    # TODO: Typehint return
+        self.cost_weights = cost_weights
+        self.alpha = alpha
+        self.gamma = gamma
+
     @torch.no_grad()
-    def forward(self, predictions: Dict[str, Tensor], targets: List[Dict[str, Tensor]]):
-        """ """
-        pass
+    def forward(self, predictions: Dict[str, Tensor], targets: Targets) -> Matches:
+        """
+        Perform Hungarian matching between predictions and targets.
+
+        Args:
+            predictions: Model predictions, with keys:
+                - `logits`: Class logits of shape (batch_size, num_queries, num_classes).
+                - `boxes`: Predicted bounding boxes of shape (batch_size, num_queries, 4).
+            targets: List of targets for each image, with keys:
+                - `labels`: Target class labels of shape (num_targets,).
+                - `boxes`: Target bounding boxes of shape (num_targets, 4).
+
+        Returns:
+            matched_indices: Matched prediction and target indices for each image in the batch.
+        """
+
+        batch_size, _, _ = predictions["logits"].shape
+
+        # TODO: Evaluate if batch processing is more efficient
+        matched_indices = []
+
+        for i in range(batch_size):
+            # Retrieve image predictions and ground truths
+            prediction_logits = predictions["logits"][i]
+            prediction_boxes = predictions["boxes"][i]
+            target_labels = targets[i]["labels"]
+            target_boxes = targets[i]["boxes"]
+
+            # Calculate individual costs
+            box_cost, giou_cost = self._calculate_box_costs(prediction_boxes, target_boxes)
+            class_cost = self._calculate_class_cost(prediction_logits, target_labels)
+
+            # Weighted sum (num_predictions, num_targets)
+            costs = {"class": class_cost, "box": box_cost, "giou": giou_cost}
+            total_cost = sum(self.cost_weights.get(k, 1) * v for k, v in costs.items())
+
+            # Handle NaN and infinite values
+            total_cost.nan_to_num_(nan=1e6, posinf=1e6, neginf=-1e6)
+            total_cost.clamp_(-1e6, 1e6)
+
+            # Solve the linear sum assignment problem
+            prediction_indices, target_indices = linear_sum_assignment(total_cost.cpu())
+
+            # Store the prediction and target indices for the image
+            prediction_indices = torch.as_tensor(prediction_indices, dtype=torch.int64, device=total_cost.device)
+            target_indices = torch.as_tensor(target_indices, dtype=torch.int64, device=total_cost.device)
+            matched_indices.append((prediction_indices, target_indices))
+
+        return matched_indices
+
+    def _calculate_box_costs(self, prediction_boxes: Tensor, target_boxes: Tensor) -> Tensor:
+        """
+        Calculate L1 and GIoU costs between prediction and target boxes.
+
+        Args:
+            prediction_boxes: Predicted bounding boxes of shape (num_predictions, 4).
+            target_boxes: Target bounding boxes of shape (num_targets, 4).
+
+        Returns:
+           box_cost: L1 distance between prediction and target boxes.
+           #### giou_cost
+           Generalized IoU cost between prediction and target boxes.
+        """
+
+        # Minimize L1 Distance
+        box_cost = torch.cdist(prediction_boxes, target_boxes, p=1)
+
+        # Maximize GIoU
+        prediction_boxes = box_convert(prediction_boxes, "cxcywh", "xyxy")
+        target_boxes = box_convert(target_boxes, "cxcywh", "xyxy")
+
+        giou_cost = -generalized_box_iou(prediction_boxes, target_boxes)
+
+        return box_cost, giou_cost
+
+    def _calculate_class_cost(self, prediction_logits: Tensor, target_labels: Tensor) -> Tensor:
+        """
+        Calculate classification cost between predicted logits and target labels.
+
+        Args:
+            prediction_logits: Predicted class logits of shape (num_predictions, num_classes).
+            target_labels: Target class labels of shape (num_targets,).
+
+        Returns:
+            class_cost: Classification cost between predicted logits and target labels.
+        """
+
+        # We use binary focal loss for classification
+        prediction_probs = prediction_logits.sigmoid()
+
+        # Numerically stable version of α * (1 - prob) ** γ * -prob.log()
+        pos_class_cost = self.alpha * ((1 - prediction_probs) ** self.gamma) * (-F.logsigmoid(prediction_logits))
+
+        # Numerically stable version of (1 - α) * prob ** γ * -(1 - prob).log()
+        neg_class_cost = (1 - self.alpha) * (prediction_probs**self.gamma) * (-F.logsigmoid(-prediction_logits))
+
+        class_cost = pos_class_cost[..., target_labels] - neg_class_cost[..., target_labels]  # (num_preds, num_classes)
+
+        return class_cost
 
     @take_annotation_from(forward)
     def __call__(self, *args, **kwargs):
