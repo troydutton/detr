2ea5d617110011ae4f94a86edb28fdd95c44efa3
Troy Dutton <troydutton@utexas.edu>
2026-03-10 12:58:02 -0500

    chore: Make setup script idempotent

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,9 +6,12 @@ set -eou pipefail
 ENV_NAME="detr"
 
 # Environment creation
+if conda env list | grep -q "^${ENV_NAME}[[:space:]]"; then
+    conda env remove -y --name "$ENV_NAME"
+fi
 conda env create -f environment.yaml
 
-# We source conda.sh to bring 'activate' into this shell's scope
+# Add lib to the search path
 CONDA_PATH=$(conda info --base)
 source "$CONDA_PATH/etc/profile.d/conda.sh"
 conda activate "$ENV_NAME"
