0634363304a6f367776f55c97b41369fff0086b5
Troy Dutton <troydutton@utexas.edu>
2026-03-10 13:05:32 -0500

    chore: Deactivate environment before setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,15 +5,18 @@ set -eou pipefail
 
 ENV_NAME="detr"
 
+# Bring conda into the path
+CONDA_PATH=$(conda info --base)
+source "$CONDA_PATH/etc/profile.d/conda.sh"
+
 # Environment creation
+conda deactivate
 if conda env list | grep -q "^${ENV_NAME}[[:space:]]"; then
     conda env remove -y --name "$ENV_NAME"
 fi
 conda env create -f environment.yaml
 
 # Add lib to the search path
-CONDA_PATH=$(conda info --base)
-source "$CONDA_PATH/etc/profile.d/conda.sh"
 conda activate "$ENV_NAME"
 conda env config vars set LD_LIBRARY_PATH=$CONDA_PREFIX/lib
 
