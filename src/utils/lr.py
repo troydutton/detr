5952c6d6962752a9d7ada02a9efe181f6b7cf2ff
Troy Dutton <troydutton@utexas.edu>
2026-04-06 23:43:03 -0500

    feat: Implement cosine learning rate scheduler

--- src/utils/lr.py
+++ src/utils/lr.py
@@ -20,6 +20,8 @@ def prepare_scheduler_arguments(args: Args, steps_per_epoch: int) -> Args:
 
     if name == "StepLR":
         args["step_size"] *= steps_per_epoch
+    elif name == "CosineAnnealingLR":
+        args["T_max"] *= steps_per_epoch
     else:
         raise NotImplementedError(f"Scheduler '{name}' not supported yet.")
 
