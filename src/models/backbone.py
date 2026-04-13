628ed51068a7c4038628d78b1393ba0d245346aa
Troy Dutton <troydutton@utexas.edu>
2026-04-13 11:39:08 -0500

    feat: Integrate DINOv2

--- src/models/backbone.py
+++ src/models/backbone.py
@@ -1,13 +1,12 @@
 import logging
 from dataclasses import dataclass
-from typing import List, Protocol
+from typing import List
 
-import timm
 import torch
 import torch.nn as nn
-from timm.models import FeatureInfo
 from torch import Tensor
 
+from models.dinov2 import Dinov2WithRegistersConfig, Dinov2WithRegistersModel
 from models.layers.positional_embedding import build_pos_embed
 from models.projector import Projector
 from utils.misc import take_annotation_from
@@ -27,9 +26,9 @@ class Features:
 class Backbone(nn.Module):
     """
     Args:
-        name: Name of the backbone to load from timm.
+        name: Name of the backbone (e.g. dinov2_vits14).
         embed_dim: Dimension of the output embeddings.
-        num_levels: Number of feature levels to extract from the backbone, optional.
+        out_feature_indices: Indices of the stages from which features should be extracted.
         pretrained: Whether to load pretrained weights, optional.
         **kwargs: Arguments to construct the projector. See `models.projector.Projector`.
     """
@@ -38,9 +37,8 @@ class Backbone(nn.Module):
         self,
         name: str,
         embed_dim: int,
-        num_levels: int = 1,
+        out_feature_indices: List[int] = [-1],
         *,
-        downsampled_feature: bool = False,
         enable_projector: bool = False,
         enable_level_pos: bool = False,
         pretrained: bool = True,
@@ -48,33 +46,23 @@ class Backbone(nn.Module):
     ) -> None:
         super().__init__()
 
-        assert not (enable_projector and downsampled_feature), "Projector and downsampled feature cannot be enabled at the same time."
-
         self.embed_dim = embed_dim
-        self.num_levels = num_levels
-        self.downsampled_feature = downsampled_feature
+        self.out_feature_indices = out_feature_indices
         self.enable_projector = enable_projector
 
-        # Take the last num_levels features from the backbone, taking into account whether
-        # we will add an extra downsampled feature from the final backbone feature map
-        out_indices = list(range(-(num_levels - 1 if downsampled_feature else num_levels), 0))
+        # Configure DINOv2
+        config = Dinov2WithRegistersConfig.from_pretrained(name)
+        config.image_size = 512
+        config.patch_size = 16
+        self.backbone = Dinov2WithRegistersModel(config, out_feature_indices=out_feature_indices)
 
-        # Build the backbone in feature-only mode
-        self.backbone: BackboneType = timm.create_model(
-            name,
-            features_only=True,
-            out_indices=out_indices,
-            pretrained=pretrained,
-        ).to(memory_format=torch.channels_last)
-        feature_channels = [self.backbone.feature_info[i]["num_chs"] for i in out_indices]
-        feature_strides = [self.backbone.feature_info[i]["reduction"] for i in out_indices]
+        feature_channels = [config.hidden_size for _ in out_feature_indices]
+        feature_strides = [config.patch_size for _ in out_feature_indices]
 
         # Create projections from each backbone output to the desired embedding dimension
         self.projections = nn.ModuleList()
         for in_channels in feature_channels:
             self.projections.append(nn.Conv2d(in_channels, embed_dim, kernel_size=1))
-        if downsampled_feature:
-            self.projections.append(nn.Conv2d(in_channels, embed_dim, kernel_size=3, stride=2, padding=1))
         self.projections.to(memory_format=torch.channels_last)
 
         # Initialize the multi-scale projector if requested
@@ -82,7 +70,8 @@ class Backbone(nn.Module):
             self.projector: Projector = Projector(in_strides=feature_strides, **kwargs["projector"]).to(memory_format=torch.channels_last)
 
         # Per-level positional embeddings
-        self.num_output_levels = self.projector.num_output_levels if enable_projector else num_levels
+        self.num_output_levels = self.projector.num_output_levels if enable_projector else len(out_feature_indices)
+
         self.level_pos = nn.Embedding(self.num_output_levels, embed_dim) if enable_level_pos else None
 
         self._initialize_weights()
@@ -93,20 +82,11 @@ class Backbone(nn.Module):
             images: Image with shape (batch_size, 3, height, width).
 
         Returns:
-            features: Multi-level features with the following fields
-            - `embed`: Embeddings with shape (batch_size, num_features, embed_dim).
-            - `pos`: Positional embeddings with shape (batch_size, num_features, embed_dim).
-            - `reference`: Reference points for the features with shape (batch_size, num_features, 2).
-            - `levels`: Level index for each feature with shape (num_features,).
-            - `dimensions`: Width and height of each feature level with shape (num_levels, 2).
+            features: Multi-level features
         """
 
         # Extract backbone features
-        backbone_features = self.backbone(images)  # List of features with shape (batch_size, channels, feature_height, feature_width)
-
-        # Optionally, create an additional downsampled feature from the final backbone feature.
-        if self.downsampled_feature:
-            backbone_features.append(backbone_features[-1])
+        backbone_features = self.backbone(images)
 
         # Project all features to the desired embedding dimension
         features: List[Tensor] = [projection(feature) for projection, feature in zip(self.projections, backbone_features)]
@@ -181,10 +161,3 @@ class Backbone(nn.Module):
     @take_annotation_from(forward)
     def __call__(self, *args, **kwargs):
         return nn.Module.__call__(self, *args, **kwargs)
-
-
-class BackboneType(Protocol):
-    feature_info: FeatureInfo
-
-    def to(self, *args, **kwargs) -> "BackboneType": ...
-    def __call__(self, Images: Tensor) -> List[Tensor]: ...
